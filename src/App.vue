2150b4095b139246afa8a6b67c8c5ef70d4c2bc5
“yangliu10062” <yangliu10062@hellobike.com>
2020-05-14 10:28:43 +0800

    字体

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,9 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  width: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="hello">
     <div class="first">
-
+      <div class="top-text">
+        <span class="green">"</span>
+        <span class="white">You are fired</span>
+        <span class="green">"</span>
+      </div>
+      <div class="middle-text">
+        <span>IMMJ - Story / Project A</span>
+      </div>
+      <div class="bottom-text">
+        <span>Tutor: Tongfei & Sharron</span>
+      </div>      
     </div>
   </div>
 </template>
@@ -16,9 +26,28 @@ export default {
 <style lang="less" scoped>
 .hello {
   .first {
-    width: 100%;
-    // height: 100px;
-    background-image: url(../images/01.jpg)
+    height: 738px;
+    background-image: url(../images/01.jpg);
+    background-repeat: no-repeat;
+    padding: 435px 305px 0 305px;
+    font-family: fangzheng;
+    .top-text {
+      text-align: center;
+      .green {
+        color:aquamarine;
+      }
+      .white {
+        color: white;
+      }
+    }
+    .middle-text {
+      text-align: center;
+      color: white;
+    }
+    .bottom-text {
+      text-align: center;
+      color: white;
+    }
   }
 }
 </style>
